bf272946e5bee1f88424669b3c1264762268e312
Swantan Barua <work.swantan@gmail.com>
2024-04-21 16:40:22 +0530

    Cell structure defined

---
+++ Uber-UIKit/UI/XIBs/TableViewCells/EnterLocationPromptCell.swift
@@ -0,0 +1,24 @@
+//
+//  EnterLocationPromptCell.swift
+//  Uber-UIKit
+//
+//  Created by Swantan Barua on 21/04/24.
+//
+
+import UIKit
+
+class EnterLocationPromptCell: UITableViewCell {
+
+    // MARK: - IBOUTLETS
+    
+    // MARK: - LIFECYCLE METHODS
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: - IBACTIONS
+}
