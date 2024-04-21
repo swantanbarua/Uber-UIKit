9456dbe80f9a04434afd4c9b20142aff54f21dcd
Swantan Barua <work.swantan@gmail.com>
2024-04-21 16:51:55 +0530

    Enums added

---
+++ Uber-UIKit/Resources/Helpers/Enums/TableViewCellIdentifierEnums.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCellIdentifierEnums.swift
+//  Uber-UIKit
+//
+//  Created by Swantan Barua on 21/04/24.
+//
+
+import Foundation
+
+enum TableViewCellIdentifierEnums {
+    
+    case EnterLocationPromptCell
+}
