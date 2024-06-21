d26bc97bd11cc0f2c6a661bc68c198df46ca7708
Swantan Barua <work.swantan@gmail.com>
2024-06-21 22:21:42 +0530

    SuggestedDestinationCell added

---
+++ Uber-UIKit/MVVM/View/Cells/TableViewCells/SuggestedDestinationCell.swift
@@ -0,0 +1,22 @@
+//
+//  SuggestedDestinationCell.swift
+//  Uber-UIKit
+//
+//  Created by Swantan Barua on 21/06/24.
+//
+
+import UIKit
+
+class SuggestedDestinationCell: UITableViewCell {
+
+    // MARK: - IBOUTLETS
+    
+    // MARK: - LIFECYCLE
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
