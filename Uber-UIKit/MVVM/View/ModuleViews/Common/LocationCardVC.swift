e4be8bf9dc15389565165f23961c5564ea6577ec
Swantan Barua <work.swantan@gmail.com>
2024-06-22 11:50:08 +0530

    LocationCardVC created

---
+++ Uber-UIKit/MVVM/View/ModuleViews/Common/LocationCardVC.swift
@@ -0,0 +1,20 @@
+//
+//  LocationCardVC.swift
+//  Uber-UIKit
+//
+//  Created by Swantan Barua on 22/06/24.
+//
+
+import UIKit
+
+class LocationCardVC: UIViewController {
+
+    // MARK: - IBOUTLETS
+    
+    // MARK: - VIEWCONTROLLER LIFECYCLE METHODS
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - IBACTIONS
+}
