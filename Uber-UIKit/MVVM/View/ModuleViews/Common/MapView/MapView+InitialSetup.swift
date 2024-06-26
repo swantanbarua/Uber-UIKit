b1ea7199973edd349c69a59551bd1c94c7727d11
Swantan Barua <work.swantan@gmail.com>
2024-06-26 18:23:03 +0530

    Add lifecycle methods for MapView

---
+++ Uber-UIKit/MVVM/View/ModuleViews/Common/MapView/MapView+InitialSetup.swift
@@ -0,0 +1,15 @@
+//
+//  MapView+InitialSetup.swift
+//  Uber-UIKit
+//
+//  Created by Swantan Barua on 26/06/24.
+//
+
+import UIKit
+
+extension MapView {
+    
+    internal func setupView() {
+        
+    }
+}
--- Uber-UIKit/MVVM/View/ModuleViews/Common/MapView/MapView.swift
+++ Uber-UIKit/MVVM/View/ModuleViews/Common/MapView/MapView.swift
@@ -18,4 +18,15 @@ class MapView: UIView, MKMapViewDelegate, CLLocationManagerDelegate {
     
     private var destinationCoordinate: CLLocationCoordinate2D?
     
+    // MARK: - VIEWCONTROLLER LIFECYCLE METHODS
+    
+    // INIT
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
