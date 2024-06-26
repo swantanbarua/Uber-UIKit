73f3bee5eb5de10d727b0f5c205a6ffa4035827f
Swantan Barua <work.swantan@gmail.com>
2024-06-26 19:16:10 +0530

    CommuteSmarterCell added

---
+++ Uber-UIKit/MVVM/View/Cells/TableViewCells/HomeCells/CommuteSmarter/CommuteSmarterCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommuteSmarterCell.swift
+//  Uber-UIKit
+//
+//  Created by Swantan Barua on 26/06/24.
+//
+
+import UIKit
+
+class CommuteSmarterCell: UITableViewCell {
+
+    // MARK: - IBOUTLETS
+    
+    // MARK: - LIFECYCLE METHODS
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: - IBACTIONS
+}
