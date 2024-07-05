7417b5a9a39d02395d94d71a5070dfbc9041fd2f
Swantan Barua <work.swantan@gmail.com>
2024-07-05 21:52:12 +0530

    Comments added for SuggestedVehiclesView

--- Uber-UIKit/MVVM/View/ModuleViews/Common/CustomViews/SuggestedVehiclesView.swift
+++ Uber-UIKit/MVVM/View/ModuleViews/Common/CustomViews/SuggestedVehiclesView.swift
@@ -9,21 +9,29 @@ import UIKit
 
 final class SuggestedVehiclesView: UIView {
     
+    // Initializer when creating the view programmatically
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureView()
     }
     
+    // Initializer when loading the view from a storyboard or nib file
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         configureView()
     }
     
+    // Configures the view by loading it from the corresponding nib file
     private func configureView() {
+        // Attempt to load the view from the nib file specified by CustomNibEnums.SuggestedVehiclesView
         guard let view = self.loadViewFromNib(
             nibName: CustomNibEnums.SuggestedVehiclesView.rawValue
         ) else { return }
+        
+        // Set the frame of the loaded view to match the bounds of the current view
         view.frame = self.bounds
+        
+        // Add the loaded view as a subview to the current view
         self.addSubview(view)
     }
 }
