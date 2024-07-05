8576ef66c69a48f5338a84215dce09416b0e1b70
Swantan Barua <work.swantan@gmail.com>
2024-07-05 21:41:46 +0530

    Wrote extension function to load view from nib file

--- Uber-UIKit/Resources/Helpers/Extensions/UIView+Extension.swift
+++ Uber-UIKit/Resources/Helpers/Extensions/UIView+Extension.swift
@@ -30,3 +30,19 @@ extension UIView {
         self.layer.masksToBounds = false
     }
 }
+
+extension UIView {
+    
+    func loadViewFromNib(nibName: String) -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(
+            nibName: nibName,
+            bundle: bundle
+        )
+        
+        return nib.instantiate(
+            withOwner: self,
+            options: nil
+        ).first as? UIView
+    }
+}
